d00ed1021a286c9027d7343c756da7a5c5d3dd90
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-01-16 16:02:11 -0300

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let limitHigh = haystack.length;
+    let limitLow = 0;
+    let middleIndex;
+    while (limitHigh > limitLow) {
+        middleIndex = limitLow + Math.floor((limitHigh - limitLow) / 2)
+        if (haystack[middleIndex] === needle) {
+            return true;
+        } else if (haystack[middleIndex] > needle) {
+            limitHigh = middleIndex;
+        } else {
+            limitLow = middleIndex + 1;
+        }
+    }
+    return false;
 }
