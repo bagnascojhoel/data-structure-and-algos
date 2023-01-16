99bf5b9f7a211a4ea6048f99f524588766d1d664
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-01-16 19:17:02 -0300

    change binary search to use inclusive bounds

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,13 +1,13 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    let limitHigh = haystack.length;
+    let limitHigh = haystack.length -1;
     let limitLow = 0;
     let middleIndex;
-    while (limitHigh > limitLow) {
+    while (limitHigh >= limitLow) {
         middleIndex = limitLow + Math.floor((limitHigh - limitLow) / 2)
         if (haystack[middleIndex] === needle) {
             return true;
         } else if (haystack[middleIndex] > needle) {
-            limitHigh = middleIndex;
+            limitHigh = middleIndex -1;
         } else {
             limitLow = middleIndex + 1;
         }
