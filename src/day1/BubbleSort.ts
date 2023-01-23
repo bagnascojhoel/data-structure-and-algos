3f9f6e14b435e3dea26b53fbf4cc378343280598
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-01-23 16:46:37 -0300

    implement queue

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length -1 -i; j++) {
+            const current = arr[j];
+            const next = arr[j + 1];
+            if (current > next) {
+                arr[j + 1] = current;
+                arr[j] = next; 
+            }
+        }
+    }
 }
--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,54 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>
+    previous?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const newNode = {
+            value: item,
+            next: undefined,
+            previous: this.tail
+        }
+        if (this.tail) {
+            this.tail.next = newNode;
+        }
+        if (this.head === undefined) {
+            this.head = newNode;
+        }
+        this.tail = newNode;
+    }
 
-}
     deque(): T | undefined {
+        const dequeuedNode = this.head;
+        if (dequeuedNode) {
+            this.length--;
+            if (dequeuedNode.next) {
+                dequeuedNode.next.previous = undefined;
+                this.head = dequeuedNode.next;
+                dequeuedNode.next = undefined;
+            } else {
+                this.head = undefined;
+            }
+        }
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
 
-}
+        return dequeuedNode?.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
