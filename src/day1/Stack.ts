fdfd0f6b1b5b6cf6334f213a18f22af76db68b1b
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-01-23 16:59:57 -0300

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>
+    prev?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        const newNode = {value: item} as Node<T>
+        if (this.head !== undefined) {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+        }
+        this.head = newNode;
+    }
 
-}
     pop(): T | undefined {
+        if (this.head === undefined) {
+            return undefined;
+        }
+        this.length--;
+        const popped = this.head;
+        const next = popped.next;
+        if (next !== undefined) {
+            next.prev = undefined;
+            popped.next = undefined;
+        }
+        this.head = next;
+        return popped.value;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
