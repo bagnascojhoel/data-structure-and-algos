6f7dc63dc194d0a5eecdbee530232a1155e558ff
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-01-25 16:40:37 -0300

    implement ring buffer

--- src/__tests__/RingBuffer.ts
+++ src/__tests__/RingBuffer.ts
@@ -12,13 +12,12 @@ test("RingBuffer", function() {
     expect(buffer.pop()).toEqual(42);
     expect(buffer.pop()).toEqual(9);
     expect(buffer.pop()).toEqual(undefined);
-
+;
     buffer.push(42);
     buffer.push(9);
-    buffer.push(12);
+    buffer.push(12);;
     expect(buffer.get(2)).toEqual(12);
     expect(buffer.get(1)).toEqual(9);
     expect(buffer.get(0)).toEqual(42);
 });
-
 
