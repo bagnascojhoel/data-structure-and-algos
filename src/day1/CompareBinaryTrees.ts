53353ea50531b35e175ef7bc64f76f88af9766c0
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-02-15 21:52:12 -0300

    implement compary binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,10 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(a: BinaryNode<number> | null | undefined, b: BinaryNode<number> | null | undefined): boolean {
+  if (!a && !b) return true;
+  if ((a && !b) || (!a && b)) return false;
+  if (a?.value !== b?.value) return false;
 
+  let leftIsEqual = compare(a?.left, b?.left);
+  let rightIsEqual = compare(a?.right, b?.right);
+
+  return leftIsEqual && rightIsEqual;
 }
