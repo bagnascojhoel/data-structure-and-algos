9d193c17fb50387f4946cfd230e075c60a08da47
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-02-16 23:27:44 -0300

    implement binary tree traversal post-order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,8 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+  const result: number[] = []
+  if (head.left) result.push(...in_order_search(head.left));
+  result.push(head.value);
+  if (head.right) result.push(...in_order_search(head.right));
 
+  return result;
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,9 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+  const result: number[] = [];
 
+  if (head.left) result.push(...post_order_search(head.left));
+  if (head.right) result.push(...post_order_search(head.right));
+  result.push(head.value);
+
+  return result;
 }
