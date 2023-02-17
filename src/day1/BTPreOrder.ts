14207ef9709de54af00c9c593020c4f3f2f0b7cb
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-02-16 23:18:57 -0300

    implement binary tree traversal pre-order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,6 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-
+  const result: number[] = [head.value];
+  if (head.left) result.push(...pre_order_search(head.left))
+  if (head.right) result.push(...pre_order_search(head.right))
+  return result;
 }
