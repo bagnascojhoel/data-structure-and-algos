4aecc0d40d2ea3976982257a5204d5cc26ae870f
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-02-16 23:41:53 -0300

    improve binary tree comparison

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,7 +1,7 @@
 export default function compare(a: BinaryNode<number> | null | undefined, b: BinaryNode<number> | null | undefined): boolean {
   if (!a && !b) return true;
-  if ((a && !b) || (!a && b)) return false;
-  if (a?.value !== b?.value) return false;
+  if (!a || !b) return false;
+  if (a.value !== b.value) return false;
 
   let leftIsEqual = compare(a?.left, b?.left);
   let rightIsEqual = compare(a?.right, b?.right);
