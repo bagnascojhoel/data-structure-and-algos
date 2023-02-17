981b864c012cc50b3cc9ea987fc6de23ce480be6
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-02-16 23:35:26 -0300

    implement binary tree breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,11 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  if (head.value === needle) return true;
 
+  const left = head.left?.value === needle;
+  const right = head.right?.value === needle;
+  if (left || right) return true;
+
+  const leftSearch = head.left ? bfs(head.left, needle) : false
+  const rightSearch = head.right ? bfs(head.right, needle) : false;
+  return leftSearch || rightSearch;
 }
